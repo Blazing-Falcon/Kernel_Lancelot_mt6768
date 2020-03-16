853ee2682cccd88501a0d5e416d1cdb2ca2c41f5
Rapherion Rollerscaperers <rapherion@raphielgang.org>
2020-03-16 23:30:51 +0700

    touchscreen: mediatek/nt36672: Minor code fix
    Change-Id: If799b1022a0a6f33a8e88c84a51bee1cbf5d4fd6
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>

--- drivers/input/touchscreen/mediatek/NT36672/nt36672.c
+++ drivers/input/touchscreen/mediatek/NT36672/nt36672.c
@@ -604,7 +604,6 @@ return:
 int32_t nvt_read_pid(void)
 {
 	uint8_t buf[4] = {0};
-	int32_t ret = 0;
 
 	//---set xdata index to EVENT BUF ADDR---
 	nvt_set_page(ts->mmap->EVENT_BUF_ADDR | EVENT_MAP_PROJECTID);
@@ -622,7 +621,7 @@ int32_t nvt_read_pid(void)
 
 	NVT_LOG("PID=%04X\n", ts->nvt_pid);
 
-	return ret;
+	return 0;
 }
 
 /*******************************************************
@@ -1766,7 +1765,7 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 #endif
 
 #if WAKEUP_GESTURE
-	for (retry = 0; retry < (sizeof(gesture_key_array) / sizeof(gesture_key_array[0])); retry++) {
+	for (retry = 0; retry < ARRAY_SIZE(gesture_key_array); retry++) {
 		input_set_capability(ts->input_dev, EV_KEY, gesture_key_array[retry]);
 	}
 	ts->input_dev->event = nvt_gesture_switch;
