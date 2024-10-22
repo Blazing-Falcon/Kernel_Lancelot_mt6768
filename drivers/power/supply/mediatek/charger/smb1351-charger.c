58a663f8bb31e8d18671d72002eface509d80704
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2024-10-22 20:26:58 +0300

    Use power efficient workingqueues for smb1351-charger

--- drivers/power/supply/mediatek/charger/smb1351-charger.c
+++ drivers/power/supply/mediatek/charger/smb1351-charger.c
@@ -1108,14 +1108,14 @@ retry_apsd:
 			chip->chg_type = STANDARD_CHARGER;
 			if (chip->bc12_en) {
 				pr_info("bc12 enabled, schedule hvdcp work.\n");
-				schedule_delayed_work(&chip->chg_hvdcp_det_work,
+				queue_delayed_work(system_power_efficient_wq, &chip->chg_hvdcp_det_work,
 							msecs_to_jiffies(HVDCP_NOTIFY_MS));
 			}
-			schedule_delayed_work(&chip->sw_rerun_aicl_work, msecs_to_jiffies(6000));
+			queue_delayed_work(system_power_efficient_wq, &chip->sw_rerun_aicl_work, msecs_to_jiffies(6000));
 			break;
 		case STATUS_PORT_SDP:
 			chip->chg_type = STANDARD_HOST;
-			schedule_delayed_work(&chip->float_chg_det_work, msecs_to_jiffies(SDP_NOTIFY_MS));
+			queue_delayed_work(system_power_efficient_wq, &chip->float_chg_det_work, msecs_to_jiffies(SDP_NOTIFY_MS));
 			break;
 		// case STATUS_PORT_OTHER:
 		// 	chip->chg_type = STANDARD_CHARGER;
@@ -1211,7 +1211,7 @@ static void smb1351_hvdcp_500_mode_check_work(struct work_struct *work)
 	u8 usb_mode;
 	int rc;
 	if (thermal_is_500 == true) {
-		schedule_delayed_work(&chip->hvdcp_500_mode_check_work, msecs_to_jiffies(5000));
+		queue_delayed_work(system_power_efficient_wq, &chip->hvdcp_500_mode_check_work, msecs_to_jiffies(5000));
 		return;
 	}
 	rc = smb1351_read_reg(chip, STATUS_0_REG, &usb_mode);
@@ -1233,7 +1233,7 @@ static void smb1351_hvdcp_500_mode_check_work(struct work_struct *work)
 	} else {
 		hvdcp_rerun_aicl_count = 0;
 	}
-	schedule_delayed_work(&chip->hvdcp_500_mode_check_work, msecs_to_jiffies(5000));
+	queue_delayed_work(system_power_efficient_wq, &chip->hvdcp_500_mode_check_work, msecs_to_jiffies(5000));
 }
 
 static void smb1351_sw_rerun_aicl_work(struct work_struct *work)
@@ -1260,7 +1260,7 @@ static void smb1351_sw_rerun_aicl_work(struct work_struct *work)
 		smb1351_rerun_aicl(chip);
 	}
 
-	schedule_delayed_work(&chip->sw_rerun_aicl_work, msecs_to_jiffies(120000));
+	queue_delayed_work(system_power_efficient_wq, &chip->sw_rerun_aicl_work, msecs_to_jiffies(120000));
 }
 
 static void smb1351_chg_hvdcp_det_work(struct work_struct *work)
@@ -1293,7 +1293,7 @@ static void smb1351_chg_hvdcp_det_work(struct work_struct *work)
 		hvdcp_det_status = 1;
 		chip->mt_chg->usb_desc.type = POWER_SUPPLY_TYPE_USB_HVDCP;
 		power_supply_changed(chip->usb_psy);
-		schedule_delayed_work(&chip->hvdcp_500_mode_check_work, msecs_to_jiffies(5000));
+		queue_delayed_work(system_power_efficient_wq, &chip->hvdcp_500_mode_check_work, msecs_to_jiffies(5000));
 	}
 	pr_err("QC charger detected. hvdcp= %x.\n",	hvdcp_status);
 	rc = smb1351_read_reg(chip, IRQ_H_REG, &hvdcp_result);
@@ -1309,12 +1309,12 @@ static void smb1351_chg_hvdcp_det_work(struct work_struct *work)
 		chip->mt_chg->usb_desc.type = POWER_SUPPLY_TYPE_USB_HVDCP_3;
 		power_supply_changed(chip->usb_psy);
 		smb1351_set_hvdcp_dpdm(chip->chg_dev);
-		schedule_delayed_work(&chip->hvdcp_500_mode_check_work, msecs_to_jiffies(5000));
+		queue_delayed_work(system_power_efficient_wq, &chip->hvdcp_500_mode_check_work, msecs_to_jiffies(5000));
 	}
 	pr_err("QC3 hvdcp_result = 0x%x det_count = %d.\n", hvdcp_result, hvdcp_det_count);
 	if (hvdcp_det_status == 0 && hvdcp_det_count < 3) {
 		hvdcp_det_count++;
-		schedule_delayed_work(&chip->chg_hvdcp_det_work,
+		queue_delayed_work(system_power_efficient_wq, &chip->chg_hvdcp_det_work,
 							msecs_to_jiffies(HVDCP_NOTIFY_MS));
 	}
 }
@@ -2476,7 +2476,7 @@ static int smb1351_plug_out(struct charger_device *chg_dev)
 //	cm->wireless_status = WIRELESS_NULL;
 	chip->rerun_apsd_count = 0;
 //	_smb1351_enable_hvdcp_det(chip, true);
-	schedule_delayed_work(&chip->enable_hvdcp_work, msecs_to_jiffies(3000));
+	queue_delayed_work(system_power_efficient_wq, &chip->enable_hvdcp_work, msecs_to_jiffies(3000));
 	cancel_delayed_work_sync(&chip->hvdcp_500_mode_check_work);
 	/* Disable SW conn therm Regulation */
 //	rc = smblib_set_sw_conn_therm_regulation(chip, false);
@@ -2796,7 +2796,7 @@ static int smb1351_enable_chg_type_det(struct charger_device *chg_dev, bool en)
 		usb_det_flag = 0;
 		chip->hvdcp_dpdm_status = 0;
 		//_smb1351_enable_hvdcp_det(chip, true);
-		schedule_delayed_work(&chip->enable_hvdcp_work, msecs_to_jiffies(3000));
+		queue_delayed_work(system_power_efficient_wq, &chip->enable_hvdcp_work, msecs_to_jiffies(3000));
 		chip->mt_chg->usb_desc.type = smblib_apsd_results[chip->chg_type];
 		smb1351_psy_chg_type_changed(chip, true);
 		smb1351_set_usbsw_state(chip, USBSW_USB);
@@ -2867,7 +2867,7 @@ static int smb1351_enable_chg_type_det(struct charger_device *chg_dev, bool en)
 
 out:
 	mutex_unlock(&chip->chgdet_lock);
-	schedule_delayed_work(&chip->check_type_work, msecs_to_jiffies(5000));
+	queue_delayed_work(system_power_efficient_wq, &chip->check_type_work, msecs_to_jiffies(5000));
 	pr_info("%s: out.\n", __func__);
 	if (en)
 		dump_regs(chip);
@@ -3073,7 +3073,7 @@ static void smb1351_delay_init_work(struct work_struct *work)
 #endif
 	return;
 retry:
-	schedule_delayed_work(&chip->delay_init_work,
+	queue_delayed_work(system_power_efficient_wq, &chip->delay_init_work,
 			msecs_to_jiffies(100));
 }
 
@@ -3146,7 +3146,7 @@ static int smb1351_charger_probe(struct i2c_client *client,
 		}
 	}
 
-	schedule_delayed_work(&chip->delay_init_work,
+	queue_delayed_work(system_power_efficient_wq, &chip->delay_init_work,
 			msecs_to_jiffies(100));
 	return 0;
 }
