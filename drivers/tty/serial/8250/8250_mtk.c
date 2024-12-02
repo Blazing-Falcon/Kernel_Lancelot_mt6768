19e8f61a2d022975c35d592d0bba0aff5c370eb5
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2024-12-02 23:43:35 +0300

    Silence "Fail to request uart sleep"
    I have no way to test the UART, so I'll just silence the logspam and leave the code logic unchanged.

--- drivers/tty/serial/8250/8250_mtk.c
+++ drivers/tty/serial/8250/8250_mtk.c
@@ -731,8 +731,10 @@ int mtk8250_request_to_sleep(void)
 			& MTK_UART_SLEEP_ACK_IDLE)) {
 			if (i++ >= MTK_UART_WAIT_ACK_TIMES) {
 				serial_out(up, MTK_UART_SLEEP_REQ, sleep_req);
+#ifdef CONFIG_MTK_ENG_BUILD
 				pr_info_ratelimited("UART%d SLEEP ACK Fail\n",
 					line);
+#endif
 				return -EBUSY;
 			}
 			udelay(10);
