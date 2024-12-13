25c22ee747d5d3d928bbb88de389aebc9127f939
wctrl <166127285+wctrl@users.noreply.github.com>
2024-12-13 21:12:21 +0200

    drivers/input/touchscreen/mediatek/NT36xxx_HOSTDL_SPI/nt36xxx: Silence printk spam
    Signed-off-by: wctrl <166127285+wctrl@users.noreply.github.com>

--- drivers/input/touchscreen/mediatek/NT36xxx_HOSTDL_SPI/nt36xxx.c
+++ drivers/input/touchscreen/mediatek/NT36xxx_HOSTDL_SPI/nt36xxx.c
@@ -694,7 +694,7 @@ void get_tp_info(void)
 {
 	nvt_get_fw_info();
 	sprintf(tp_version_info, "[Vendor]Tianma,[TP-IC]:NT36672A,[FW]0x%x", tp_fw_version);
-	printk("[%s]: tp_version %s\n", __func__, tp_version_info);
+	pr_debug("[%s]: tp_version %s\n", __func__, tp_version_info);
 #ifdef CONFIG_HQ_SYSFS_SUPPORT
 	hq_regiser_hw_info(HWID_CTP, tp_version_info);
 #endif
@@ -1143,18 +1143,18 @@ int __init is_lcm_detect(char *str)
 {
 	if (!(strcmp(str, "ft8719_fhdp_dsi_vdo_xinli_lcm_drv"))) {
 		is_ft_lcm = 2;
-		printk("Func:%s is_ft 2:%d", __func__, is_ft_lcm);
+		pr_debug("Func:%s is_ft 2:%d", __func__, is_ft_lcm);
 	} else if (!(strcmp(str, "nt36672D_fhdp_dsi_vdo_tianma_lcm_drv"))) {
 		is_ft_lcm = 3;
-		printk("Func:%s is_ft 3:%d", __func__, is_ft_lcm);
+		pr_debug("Func:%s is_ft 3:%d", __func__, is_ft_lcm);
 	} else if (!(strcmp(str, "nt36672A_fhdp_dsi_vdo_tianma_lcm_drv"))) {
 		is_ft_lcm = 0;
-		printk("Func:%s is_ft 0:%d", __func__, is_ft_lcm);
+		pr_debug("Func:%s is_ft 0:%d", __func__, is_ft_lcm);
 	} else if (!(strcmp(str, "nt36672A_fhdp_dsi_vdo_tianma_lcm_drv_G6"))) {
 		is_ft_lcm = 1;
-		printk("Func:%s is_ft 1:%d", __func__, is_ft_lcm);
+		pr_debug("Func:%s is_ft 1:%d", __func__, is_ft_lcm);
 	}
-	printk("Func:%s is_lcm_detect:%s", __func__, str);
+	pr_debug("Func:%s is_lcm_detect:%s", __func__, str);
 	return 0;
 }
  __setup("LCM_name=", is_lcm_detect);
@@ -1335,10 +1335,10 @@ static irqreturn_t nvt_ts_work_func(int irq, void *data)
 /*
 	//--- dump SPI buf ---
 	for (i = 0; i < 10; i++) {
-		printk("%02X %02X %02X %02X %02X %02X  ",
+		pr_debug("%02X %02X %02X %02X %02X %02X  ",
 			point_data[1+i*6], point_data[2+i*6], point_data[3+i*6], point_data[4+i*6], point_data[5+i*6], point_data[6+i*6]);
 	}
-	printk("\n");
+	pr_debug("\n");
 */
 
 #if NVT_TOUCH_WDT_RECOVERY
@@ -2341,23 +2341,23 @@ static void nvt_ts_late_resume(struct early_suspend *h)
 #ifdef CONFIG_PM
 static int nvt_ts_pm_suspend(struct device *dev)
 {
-	printk("%s:++\n", __func__);
+	pr_debug("%s:++\n", __func__);
 
 	ts->dev_pm_suspend = true;
 	reinit_completion(&ts->dev_pm_resume_completion);
 
-	printk("%s:--\n", __func__);
+	pr_debug("%s:--\n", __func__);
 	return 0;
 }
 
 static int nvt_ts_pm_resume(struct device *dev)
 {
-	printk("%s:++\n", __func__);
+	pr_debug("%s:++\n", __func__);
 
 	ts->dev_pm_suspend = false;
 	complete(&ts->dev_pm_resume_completion);
 
-	printk("%s:--\n", __func__);
+	pr_debug("%s:--\n", __func__);
 	return 0;
 }
 
@@ -2409,7 +2409,7 @@ static int32_t __init nvt_driver_init(void)
 
 	NVT_LOG("start\n");
 
-	printk("%s result  is_ft:%d", __func__, is_ft_lcm);
+	pr_debug("%s result  is_ft:%d", __func__, is_ft_lcm);
 	if (2 == is_ft_lcm)
 		return -1;
 	//---add spi driver---
