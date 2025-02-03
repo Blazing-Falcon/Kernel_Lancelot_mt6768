9cdfe41b647ef1014f4cdf5b6ac0fc3ffa3ec25f
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2025-02-03 20:56:24 +0300

    drivers: mediatek/pmic: Fix logic

--- drivers/misc/mediatek/base/power/spm/mt6768/mtk_spm_internal.c
+++ drivers/misc/mediatek/base/power/spm/mt6768/mtk_spm_internal.c
@@ -275,12 +275,11 @@ unsigned int __spm_output_wake_reason(
 
 	WARN_ON(log_size >= LOG_BUF_OUT_SZ);
 
-	if (!suspend)
-		printk_deferred("[name:spm&][SPM] %s", log_buf);
-	else {
-		aee_sram_printk("%s", log_buf);
-		printk_deferred("[name:spm&][SPM] %s", log_buf);
-	}
+	printk_deferred("[name:spm&][SPM] %s", log_buf);
+
+#ifdef CONFIG_MTK_AEE_FEATURE
+	aee_sram_printk("%s", log_buf);
+#endif
 
 	return wr;
 }
