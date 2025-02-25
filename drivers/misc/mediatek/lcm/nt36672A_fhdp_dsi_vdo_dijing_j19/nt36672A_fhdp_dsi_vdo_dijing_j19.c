63e9157a10ff8d5d6982e78b6e2016bfc7f655e5
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2025-02-25 20:19:38 +0300

    drivers/misc: mediatek: lcm: remove redundant check in REGFLAG_DELAY
    Co-Authored-By: Deepak Kumar <notwho53@gmail.com>

--- drivers/misc/mediatek/lcm/nt36672A_fhdp_dsi_vdo_dijing_j19/nt36672A_fhdp_dsi_vdo_dijing_j19.c
+++ drivers/misc/mediatek/lcm/nt36672A_fhdp_dsi_vdo_dijing_j19/nt36672A_fhdp_dsi_vdo_dijing_j19.c
@@ -194,10 +194,7 @@ static void push_table(void *cmdq, struct LCM_setting_table *table,
 		switch (cmd) {
 
 		case REGFLAG_DELAY:
-			if (table[i].count <= 10)
-				MDELAY(table[i].count);
-			else
-				MDELAY(table[i].count);
+			MDELAY(table[i].count);
 			break;
 
 		case REGFLAG_UDELAY:
