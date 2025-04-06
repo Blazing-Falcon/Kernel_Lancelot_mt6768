4d0e0ce02bf3ddbaf7c4810b66937b8f003fb391
fukiame <fukiame@proton.me>
2025-04-06 16:03:46 +0700

    drivers: mediatek: video: mt6768: mtkfb: remove redundant nt35521 lcm check
    we dont use it
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/video/mt6768/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6768/videox/mtkfb.c
@@ -3029,12 +3029,6 @@ static int mtkfb_probe(struct platform_device *pdev)
 	"prim_panel_wakelock");*/
 	/* end modify for unlock speed */
 
-	if (!strcmp(mtkfb_find_lcm_driver(),
-		"nt35521_hd_dsi_vdo_truly_rt5081_drv")) {
-		register_ccci_sys_call_back(MD_SYS1,
-			MD_DISPLAY_DYNAMIC_MIPI, mipi_clk_change);
-	}
-
 #ifdef CONFIG_LM3697_SUPPORT
 	r = mtkfb_set_rgb_point_init();
 	if (r) {
