0c28b59f2464a77d950a98b24a09a429693cc071
Blazing-Falcon <farhanqpalm123@gmail.com>
2025-10-05 05:20:21 +0700

    Revert "mm: allow oom_killer to be compiled out when DISABLE_OOM_KILLER=y"
    This reverts commit 61d57695e8897c3aca418237d175b723ba61640a.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1382,7 +1382,6 @@ static struct ctl_table vm_table[] = {
 		.proc_handler	= proc_dointvec,
 	},
 #endif
-#if !defined(CONFIG_DISABLE_OOM_KILLER)
 	{
 		.procname	= "panic_on_oom",
 		.data		= &sysctl_panic_on_oom,
@@ -1406,7 +1405,6 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
-#endif
 	{
 		.procname	= "overcommit_ratio",
 		.data		= &sysctl_overcommit_ratio,
